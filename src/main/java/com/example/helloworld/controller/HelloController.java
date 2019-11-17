c2fde4117864f7d90f43358ff66be579b2629e2e
hensby <whc0721a@vip.qq.com>
2019-11-17 03:23:29 -0600

    Initial commit

---
+++ src/main/java/com/example/helloworld/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.helloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello from Hensby333333!";
+    }
+
+}
